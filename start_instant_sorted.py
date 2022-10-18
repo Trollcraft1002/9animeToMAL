9f104b805faca3648d090b2350e25b9e3c80157a
Trollcraft1002 <trollcraft1002@gmail.com>
2022-10-18 10:07:09 +0900

    code correction

--- start_instant_sorted.py
+++ start_instant_sorted.py
@@ -5,7 +5,6 @@ from pathlib import Path
 from tqdm import tqdm
 from yattag import Doc, indent
 
-
 print("")
 print("Please make sure that the exported file is with folders enabled")
 print("Or this is gonna create incorrect results!!!")
@@ -14,21 +13,23 @@ print("")
 sleep(3)
 print("Starting in 5 seconds...")
 print("")
-for i in tqdm(range(0,5)): 
+for i in tqdm(range(0, 5)):
     sleep(1)
-    
+
 try:
-    f = Path(__file__).with_name('export.txt')
-    with f.open('r',  encoding="utf-8") as f:
+    f = Path(__file__).with_name("export.txt")
+    with f.open("r", encoding="utf-8") as f:
         matches = re.findall(r"(/)(\d+)", f.read())
 except FileNotFoundError:
     print("Please make sure export.txt exists")
     exit()
 
 try:
-    f = Path(__file__).with_name('export.txt')
-    with f.open('r',  encoding="utf-8") as f:
-        matches2 = re.findall(r"(# )([A-Za-z:0-9 !?\-\\'\"\\\’,.~\/!@#$%^&*()_\+ä;:\[\]‘é`]+)", f.read())
+    f = Path(__file__).with_name("export.txt")
+    with f.open("r", encoding="utf-8") as f:
+        matches2 = re.findall(
+            r"(# )([A-Za-z:0-9 !?\-\\'\"\\\’,.~\/!@#$%^&*()_\+ä;:\[\]‘é`]+)", f.read()
+        )
 except FileNotFoundError:
     print("Please make sure export.txt exists")
     exit()
@@ -40,13 +41,13 @@ for ids in matches:
 for names in matches2:
     animeName.append(names[1])
 
-f2 = open("./senpaii.xml", "w",  encoding="utf-8")
+f2 = open("./senpaii.xml", "w", encoding="utf-8")
 f2.write("")
 f2.close()
 
-pbar = tqdm(desc="Progress", total= len(animesID))
+pbar = tqdm(desc="Progress", total=len(animesID))
 
-i=0
+i = 0
 planned = False
 watching = False
 onHold = False
@@ -54,128 +55,121 @@ watched = False
 dropped = False
 
 try:
-               
+
     for animeID in animesID:
 
-        if(animeName[i] == "Watching"):
+        if animeName[i] == "Watching":
             watching = True
             planned = False
             onHold = False
             watched = False
             dropped = False
-            i= i + 1
-        if (animeName[i] == "Planned"):
+            i += 1
+        if animeName[i] == "Planned":
             watching = False
             planned = True
             watched = False
             onHold = False
             dropped = False
-            i= i + 1
-        if (animeName[i] == "On-Hold"):
+            i += 1
+        if animeName[i] == "On-Hold":
             watching = False
             watched = False
             planned = False
             onHold = True
             dropped = False
-            i= i + 1
-        if (animeName[i] == "Watched"):
-                watched = True
-                planned = False
-                onHold = False
-                watching = False
-                dropped = False
-                i= i + 1
-        if (animeName[i] == "Dropped"):
+            i += 1
+        if animeName[i] == "Watched":
+            watched = True
+            planned = False
+            onHold = False
+            watching = False
+            dropped = False
+            i += 1
+        if animeName[i] == "Dropped":
             watched = False
             planned = False
             onHold = False
             watching = False
             dropped = True
-            
+
         doc, tag, text = Doc().tagtext()
         try:
-            with tag('root'):
-                with tag('anime'):
-                    with tag('series_animedb_id'):
+            with tag("root"):
+                with tag("anime"):
+                    with tag("series_animedb_id"):
                         text(animeID)
-                    with tag('series_title'):
+                    with tag("series_title"):
                         text(animeName[i])
-                    with tag('series_type'):
+                    with tag("series_type"):
                         text("")
-                    with tag('series_episodes'):
+                    with tag("series_episodes"):
                         text("")
-                    with tag('my_id'):
+                    with tag("my_id"):
                         text("0")
-                    with tag('my_watched_episodes'):
-                        if(watching == True):
+                    with tag("my_watched_episodes"):
+                        if watching == True:
                             text("1")
                         else:
                             text("")
-                    with tag('my_start_date'):
+                    with tag("my_start_date"):
                         text("0000-00-00")
-                    with tag('my_finish_date'):
+                    with tag("my_finish_date"):
                         text("0000-00-00")
-                    with tag('my_rated'):
+                    with tag("my_rated"):
                         text("")
-                    with tag('my_score'):
+                    with tag("my_score"):
                         text("")
-                    with tag('my_dvd'):
+                    with tag("my_dvd"):
                         text("")
-                    with tag('my_storage'):
+                    with tag("my_storage"):
                         text("")
-                    with tag('my_status'):
-                        if(watching == True):
+                    with tag("my_status"):
+                        if watching == True:
                             text("Watching")
-                        elif(watched == True):
+                        elif watched == True:
                             text("Completed")
-                        elif(planned == True):
+                        elif planned == True:
                             text("Plan to Watch")
-                        elif(onHold == True):
+                        elif onHold == True:
                             text("On-Hold")
-                        elif(dropped == True):
-                            text("Dropped")      
-                    with tag('my_comments'):
+                        elif dropped == True:
+                            text("Dropped")
+                    with tag("my_comments"):
                         text("")
-                    with tag('my_times_watched'):
-                        text('0')
-                    with tag('my_rewatch_value'):
-                        text('Low')
-                    with tag('my_tags'):
-                        text('')
-                    with tag('my_rewatching'):
-                        text('0')
-                    with tag('my_rewatching_ep'):
-                        text('0')
-                    with tag('my_discuss'):
+                    with tag("my_times_watched"):
+                        text("0")
+                    with tag("my_rewatch_value"):
+                        text("Low")
+                    with tag("my_tags"):
+                        text("")
+                    with tag("my_rewatching"):
+                        text("0")
+                    with tag("my_rewatching_ep"):
+                        text("0")
+                    with tag("my_discuss"):
                         text("1")
-                    with tag('my_sns'):
+                    with tag("my_sns"):
                         text("default")
-                    with tag('update_on_import'):
-                        text('1')
+                    with tag("update_on_import"):
+                        text("1")
 
+                result = indent(doc.getvalue(), indentation=" " * 4, newline="\n")
 
-                result = indent(
-                doc.getvalue(),
-                indentation = ' '*4,
-                newline = '\n'
-                )
-                
-                f2 = open("./senpaii.xml", "a",  encoding="utf-8")
-                f2.write('\n')
+                f2 = open("./senpaii.xml", "a", encoding="utf-8")
+                f2.write("\n")
                 f2.write(result)
-                f2.write('\n')
+                f2.write("\n")
                 f2.close
-        except (Exception, AttributeError,TypeError,ValueError):
-                    print(' Something went wrong')
-        i = i + 1
+        except (Exception, AttributeError, TypeError, ValueError):
+            print(" Something went wrong")
+        i += 1
         pbar.update()
-         
+
 except KeyboardInterrupt:
     pbar.close()
-    print(' Downt\' leawwe me senpai')
-    print(' 😣😣😣')
+    print(" Downt' leawwe me senpai")
+    print(" 😣😣😣")
     exit()
-    
+
 pbar.close()
-#Hiwwo there... You probably are wondering why this is here... right?
-#Well... to be honest I am trying to figure why and how does this work... but it does the job done :D
